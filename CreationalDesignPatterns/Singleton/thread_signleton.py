f4660924377c102a867b21104b73ebc951853448
孔华明 <13819493696@139.com>
2020-09-17 15:37:05 +0800

    新增创建型模式，包括工厂方法、抽象工厂、生成器、原型和单例等。

---
+++ CreationalDesignPatterns/Singleton/thread_signleton.py
@@ -0,0 +1,45 @@
+from threading import Lock, Thread
+
+"""
+单例模式(线程安全单例)
+"""
+class Singleton(type):
+
+    _instances = {}
+
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Singleton(metaclass=Singleton):
+    value: str = None
+
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+    def some_business_logic(self):
+        pass
+
+
+def test_singleton(value: str) -> None:
+    singleton = Singleton(value)
+    print(singleton.value)
+
+
+if __name__ == "__main__":
+    # The client code.
+
+    print("如果看到两个相同的结果，则说明线程安全单例生效！\n"
+          "如果看到不同的结果，则说明有两个单例生成！\n\n"
+          "RESULT:\n")
+
+    process1 = Thread(target=test_singleton, args=("FOO",))
+    process2 = Thread(target=test_singleton, args=("BAR",))
+    process1.start()
+    process2.start()
